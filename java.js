c071afe91ba11b788817bb704f948446e171e269
ALOK SARKAR <97972813+aloksarkar7085@users.noreply.github.com>
2024-07-09 16:43:07 +0530

    add js

---
+++ java.js
@@ -0,0 +1,9 @@
+
+  
+    let typed = new Typed('.multiple-text',{
+      strings:['Web Developer.','Software Engineer.','Editor.','Code Lover.','DSA Lover.'],
+typeSpeed:70,
+backSpeed:70,
+backDelay:1000,
+loop:true,
+    });
